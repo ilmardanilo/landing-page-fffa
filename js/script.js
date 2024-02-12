5dfd5294fef2f638160bd536ba9a5afca82087a0
Ilmar <ilmardanilo@hotmail.com>
2024-02-12 13:04:12 -0300

    feat: add howToEnroll section

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const button = document.querySelector("button");
+
+const addLoading = () => {
+  button.innerHTML =
+    '<img src="./assets/images/loading.png" alt="loading" class="loading" / >';
+};
+
+const removeLoading = () => {
+  button.innerHTML = "SEND";
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  addLoading();
+
+  const nameOfResponsible = document.querySelector(
+    "input[name=name_of_responsible]"
+  ).value;
+  const nameOfChild = document.querySelector("input[name=name_of_child]").value;
+  const age = document.querySelector("input[name=age]").value;
+  const playingExperience = document.querySelector(
+    "input[name=playing_experience]"
+  ).value;
+  const email = document.querySelector("input[name=email]").value;
+  const freeTrialDay = document.querySelector(
+    "input[name=free_trial_day]"
+  ).value;
+
+  fetch("https://api.sheetmonkey.io/form/6s5ZiBDmQu8WhCoHU1XiNL", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name_of_responsible: nameOfResponsible,
+      name_of_child: nameOfChild,
+      age,
+      playing_experience: playingExperience,
+      email,
+      free_trial_day: freeTrialDay,
+    }),
+  }).then(() => {
+    removeLoading();
+    document.querySelector("form").reset();
+    alert("Successful registration");
+
+    const spans = document.querySelectorAll(".span-active");
+    spans.forEach((span) => span.classList.remove("span-active"));
+  });
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const inputs = document.querySelectorAll(".input");
+
+const handleFocus = ({ target }) => {
+  const span = target.previousElementSibling;
+  span.classList.add("span-active");
+};
+
+const handleFocusOut = ({ target }) => {
+  if (target.value === "") {
+    const span = target.previousElementSibling;
+    span.classList.remove("span-active");
+  }
+};
+
+inputs.forEach((input) => input.addEventListener("focus", handleFocus));
+inputs.forEach((input) => input.addEventListener("focusout", handleFocusOut));
